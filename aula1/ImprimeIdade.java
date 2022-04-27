671f960c8a40d8bbb39fcdfaf7826a26ddfaec99
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-04-27 16:52:39 -0300

    Finalização aula 1

--- aula1/ImprimeIdade.java
+++ aula1/ImprimeIdade.java
@@ -1,8 +1,7 @@
 package aula1;
 
-public class ImprimeIdade
-{
-    public static void main(String[] args) {     
+public class ImprimeIdade {
+    public static void main(String[] args) {
         // String nome = "Victor Icoma";
         // boolean acompanhado = false;
         int idade = 20;
@@ -11,23 +10,23 @@ public class ImprimeIdade
 
         double imc = (peso) / (altura * altura);
 
-        if(idade >= 40){
-            
-            if(imc < 22){
+        if (idade >= 40) {
+
+            if (imc < 22) {
                 System.out.print("IMC = " + imc + ", você está com subnutrição.");
-            }else if(imc <= 30){
+            } else if (imc <= 30) {
                 System.out.print("IMC = " + imc + ", parabéns! Você está mantendo o seu peso ideal.");
-            }else if(imc <= 35){
+            } else if (imc <= 35) {
                 System.out.print("IMC = " + imc + ", você está com sobrepeso.");
-            }else{
+            } else {
                 System.out.print("IMC = " + imc + ", você está com obesidade.");
             }
-        }else{
-            if(imc < 25){
+        } else {
+            if (imc < 25) {
                 System.out.print("IMC = " + imc + ", procure um nutricionista, você está subnutrido");
-            }else if(imc < 35){
+            } else if (imc < 35) {
                 System.out.print("IMC = " + imc + ", você está com um pouco de sobrepeso, mas no controle.");
-            }else{
+            } else {
                 System.out.print("IMC = " + imc + ", você precisa fazer mais exercícios.");
             }
         }
